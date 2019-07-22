38da712773cdba1430430ee8dce96c2bb8dc418a
Nolaxus <jonathan.jarri@nolaroads.com>
2019-07-22 18:17:14 +0200

    Fix compilation issue due to missing using in the example scripts.

--- Assets/Demo/Scripts/ExampleDynamicObject.cs
+++ Assets/Demo/Scripts/ExampleDynamicObject.cs
@@ -1,40 +1,38 @@
-﻿using dynamicscroll;
+﻿using example;
+using UnityDynamicScrollRect;
 using UnityEngine.UI;
 using UnityEngine;
 
-namespace example
+public class ExampleDynamicObject : DynamicScrollObject<ExampleData>
 {
-	public class ExampleDynamicObject : DynamicScrollObject<ExampleData>
+	public override string objectName
 	{
-		public override string objectName
+		get
 		{
-			get
-			{
-				return "ExampleObject";
-			}
+			return "ExampleObject";
 		}
+	}
 
-		private Text idText;
-		private Text nameEmailText;
-		private Text bodyText;
+	private Text idText;
+	private Text nameEmailText;
+	private Text bodyText;
 
-		public void Awake()
-		{
-			currentHeight = GetComponent<RectTransform>().rect.height;
-			currentWidth = GetComponent<RectTransform>().rect.width;
+	public void Awake()
+	{
+		currentHeight = GetComponent<RectTransform>().rect.height;
+		currentWidth = GetComponent<RectTransform>().rect.width;
 
-			idText = transform.Find("PostId").GetComponent<Text>();
-			nameEmailText = transform.Find("NameEmail").GetComponent<Text>();
-			bodyText = transform.Find("Body").GetComponent<Text>();         
-		}
+		idText = transform.Find("PostId").GetComponent<Text>();
+		nameEmailText = transform.Find("NameEmail").GetComponent<Text>();
+		bodyText = transform.Find("Body").GetComponent<Text>();         
+	}
 
-		public override void updateScrollObject(ExampleData item, int index)
-		{
-			base.updateScrollObject(item, index);
+	public override void updateScrollObject(ExampleData item, int index)
+	{
+		base.updateScrollObject(item, index);
 
-			idText.text = item.id.ToString();
-			nameEmailText.text = string.Format("{0} ({1})", item.name, item.email);
-			bodyText.text = item.body;
-		}
+		idText.text = item.id.ToString();
+		nameEmailText.text = string.Format("{0} ({1})", item.name, item.email);
+		bodyText.text = item.body;
 	}
 }
--- Assets/Demo/Scripts/ExampleScroll.cs
+++ Assets/Demo/Scripts/ExampleScroll.cs
@@ -1,32 +1,28 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
+using example;
 using UnityEngine;
-using UnityEngine.UI;
-using dynamicscroll;
+using UnityDynamicScrollRect;
 
-namespace example
+public class ExampleScroll : MonoBehaviour
 {
-	public class ExampleScroll : MonoBehaviour
+	public DynamicScrollRect verticalScroll;
+	public DynamicScrollRect horizontalScroll;
+	public GameObject referenceObject;
+
+	private ExampleData[] mData;
+	private DynamicScroll<ExampleData, ExampleDynamicObject> mVerticalDynamicScroll = new DynamicScroll<ExampleData, ExampleDynamicObject>();
+	private DynamicScroll<ExampleData, ExampleDynamicObject> mHorizontalDynamicScroll = new DynamicScroll<ExampleData, ExampleDynamicObject>();
+
+	public IEnumerator Start()
 	{
-		public DynamicScrollRect verticalScroll;
-		public DynamicScrollRect horizontalScroll;
-		public GameObject referenceObject;
+		WWW www = new WWW(@"https://jsonplaceholder.typicode.com/comments");
+		yield return www;
+		mData = JsonHelper.getJsonArray<ExampleData>(www.text);
 
-		private ExampleData[] mData;
-		private DynamicScroll<ExampleData, ExampleDynamicObject> mVerticalDynamicScroll = new DynamicScroll<ExampleData, ExampleDynamicObject>();
-		private DynamicScroll<ExampleData, ExampleDynamicObject> mHorizontalDynamicScroll = new DynamicScroll<ExampleData, ExampleDynamicObject>();
+		mVerticalDynamicScroll.spacing = 5f;
+		mVerticalDynamicScroll.Initiate(verticalScroll, mData, 0, referenceObject);
 
-		public IEnumerator Start()
-		{
-			WWW www = new WWW(@"https://jsonplaceholder.typicode.com/comments");
-			yield return www;
-			mData = JsonHelper.getJsonArray<ExampleData>(www.text);
-
-			mVerticalDynamicScroll.spacing = 5f;
-			mVerticalDynamicScroll.Initiate(verticalScroll, mData, 0, referenceObject);
-
-			mHorizontalDynamicScroll.spacing = 5f;
-			mHorizontalDynamicScroll.Initiate(horizontalScroll, mData, 0, referenceObject);
-		}
+		mHorizontalDynamicScroll.spacing = 5f;
+		mHorizontalDynamicScroll.Initiate(horizontalScroll, mData, 0, referenceObject);
 	}
 }
