bc9d0eb84507d543f04697b592d8001eeae329fc
Nolaxus <jonathan.jarri@nolaroads.com>
2019-08-05 11:27:16 +0200

    Add a grid layout in the example scene.

--- Assets/Demo/Scripts/ExampleScroll.cs
+++ Assets/Demo/Scripts/ExampleScroll.cs
@@ -7,11 +7,15 @@ public class ExampleScroll : MonoBehaviour
 {
 	public DynamicScrollRect verticalScroll;
 	public DynamicScrollRect horizontalScroll;
+	public DynamicScrollRect gridScroll;
 	public GameObject referenceObject;
+
+	public Transform GridHolder;
 
 	private ExampleData[] mData;
 	private DynamicScroll<ExampleData, ExampleDynamicObject> mVerticalDynamicScroll = new DynamicScroll<ExampleData, ExampleDynamicObject>();
 	private DynamicScroll<ExampleData, ExampleDynamicObject> mHorizontalDynamicScroll = new DynamicScroll<ExampleData, ExampleDynamicObject>();
+	private DynamicScroll<ExampleData, ExampleDynamicObject> mGridDynamicScroll = new DynamicScroll<ExampleData, ExampleDynamicObject>();
 
 	public IEnumerator Start()
 	{
@@ -24,5 +28,25 @@ public class ExampleScroll : MonoBehaviour
 
 		mHorizontalDynamicScroll.spacing = 5f;
 		mHorizontalDynamicScroll.Initiate(horizontalScroll, mData, 0, referenceObject);
+
+		mGridDynamicScroll.spacing = 5f;
+		mGridDynamicScroll.Initiate(horizontalScroll, mData, 0, referenceObject);
+
+		foreach (var item in mData)
+		{
+			GameObject exampleObject = Instantiate(referenceObject, GridHolder);
+			ExampleDynamicObject exampleDynamicObject = exampleObject.AddComponent<ExampleDynamicObject>();
+
+			ExampleData exampleData = new ExampleData
+			{
+				id = item.id,
+				name = item.name,
+				body = item.body,
+				email = item.email,
+				postId = item.postId
+			};
+
+			exampleDynamicObject.updateScrollObject(exampleData, item.id);
+		}
 	}
 }
